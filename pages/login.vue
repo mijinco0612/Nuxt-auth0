3c595be16e1dbefe4da0bdc8f190a0ad67900b86
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-06-26 20:29:11 +0900

    add login.vue

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="show-auth"/>
+</template>
+<script>
+  export default {
+    mounted() {
+      this.$auth0.showLock("show-auth");
+    }
+  };
+</script>
+<style scoped>
+  #show-auth {
+    margin-top: 20px;
+  }
+</style>
