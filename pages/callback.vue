ec910c6f08a2bb4673ea0ea93596440c02667f24
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-06-28 16:30:39 +0900

    call setTokenByQuery add redirect

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,3 +1,11 @@
 <template>
   <p>Signing in...</p>
 </template>
+<script>
+  export default {
+    mounted() {
+      this.$auth0.setTokenByQuery()
+      this.$router.replace('/')
+    }
+  };
+</script>
