278c380b46fc7086891a7c65050bf5b0f49527b4
takamichi-tatsumoto <t.t.tatsumoto@gmail.com>
2018-07-06 10:13:41 +0900

    edit index.vue add ping button

--- pages/index.vue
+++ pages/index.vue
@@ -15,15 +15,45 @@
           <span>Login</span>
         </nuxt-link>
       </div>
+      <div class="field is-grouped">
+        <p class="control">
+          <button class="button is-info is-outlined" @click="pingPublic">ping public</button>
+        </p>
+        <p class="control">
+          <button class="button is-link is-outlined" @click="pingPrivate">ping private</button>
+        </p>
+      </div>
+
+      <div class="box">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              {{message}}
+            </div>
+          </div>
+        </article>
+      </div>
+
     </div>
   </section>
 </template>
 
 <script>
   export default {
+    data: () => ({
+      message: null
+    }),
     methods: {
       loggedIn() {
         return this.$auth0.isAuthenticated();
+      },
+      async pingPublic() {
+        const returnMessage = await this.$axios.$get('/api/v1/public');
+        this.message = returnMessage;
+      },
+      async pingPrivate() {
+        const returnMessage = await this.$axios.$get('/api/v1/private');
+        this.message = returnMessage;
       }
     }
   };
